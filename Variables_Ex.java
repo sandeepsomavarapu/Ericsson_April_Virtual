69559bf242ddb9431388e2e73f6208e8e0c3a95a
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2023-04-24 12:50:35 +0530

    Day-1 Session-1

---
+++ Variables_Ex.java
@@ -0,0 +1,15 @@
+
+public class Variables_Ex {
+	boolean output=true;//global variable
+	
+	public static void main(String[] args) {
+
+		byte age = 22;
+		int salary = 9230902;
+		float marks=234.4434334308921f; 
+		double result=234.4434334308921f; //local variables
+		
+	System.out.println(age+"   "+salary+"   "+marks+"   "+result);	
+		
+	}
+}
---
+++ Welcome.java
@@ -0,0 +1,10 @@
+
+public class Welcome {
+		public static void main(String[] args) {
+			//main -->ctrl+space-->enter
+			
+			
+			
+			System.out.println("welcome to java");//syso-->ctrl+space
+		}
+}
